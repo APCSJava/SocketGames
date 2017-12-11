5744c781bc14bd00670e4df64cf3ac13ecff72ea
Kent Collins <kent.r.collins@gmail.com>
2017-12-11 04:15:46 +0000

    limited max number of connections.  logging.

--- ThreadedGameServer.java
+++ ThreadedGameServer.java
@@ -1,7 +1,9 @@
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.logging.Logger;
 
 /***
  * Spins connection requests into parallel game threads.
@@ -10,46 +12,58 @@ import java.net.Socket;
  */
 public class ThreadedGameServer {
 
+	public final static Logger LOGGER = Logger
+			.getLogger(ThreadedGameServer.class.getName());
+
 	/**
 	 * Opens a port to listen for incoming requests. When a request is received,
 	 * wraps the resulting socket connection in a new thread and starts the process.
 	 * 
-	 * args[0] the port to use when listening for connections
+	 * @param args[0]
+	 *            the port to use when listening for connections
+	 * @param args[1]
+	 *            the max number of concurrent connections to accept
 	 * 
 	 */
 	public static void main(String[] args) throws IOException {
-		// if no port number provided, use as default the value 9090
-		int desiredPort = args.length > 0 ? Integer.parseInt(args[0])
-				: 9090;
+		int desiredPort = Integer.parseInt(args[0]);
+		int maxConnections = Integer.parseInt(args[1]);
+		String refuseMessage = "Server limit of " + maxConnections
+				+ ((maxConnections == 1) ? " connection"
+						: " connections")
+				+ " has been reached.  Please try again, later.";
+
 		try (ServerSocket socketRequestListener = new ServerSocket(
 				desiredPort)) {
-			System.out.println(
-					"Server running on port " + desiredPort);
+			LOGGER.info("Server started.  Port: " + desiredPort
+					+ ".  Capacity: " + maxConnections);
 			GameTracker.initialize();
 			while (true) {
 				// the following call blocks until a connection is made
 				Socket socket = socketRequestListener.accept();
-				// verify the socket connection has been opened
-				printConnectionInfo(socket);
-				new Thread(new GameThread(socket)).start();
+				InetAddress remoteMachine = socket.getInetAddress();
+				// String remoteHost = remoteMachine.getHostName();
+				LOGGER.info("Incoming request: " + remoteMachine);
+
+				int numActiveSockets = Thread.activeCount() - 1;
+				if (numActiveSockets < maxConnections) {
+					new Thread(new GameThread(socket)).start();
+					numActiveSockets++;
+					LOGGER.info("Accepted " + remoteMachine
+							+ ".  Current connections: "
+							+ numActiveSockets);
+				} else {
+					PrintWriter out = new PrintWriter(
+							socket.getOutputStream());
+					out.println(refuseMessage);
+					out.close();
+					socket.close();
+					LOGGER.warning("Refused " + remoteMachine
+							+ ".  Current connections: "
+							+ numActiveSockets);
+				}
 			}
 		}
 	}
 
-	/**
-	 * Prints diagnostic information to the local machine (server)
-	 * 
-	 * Note the ordering of the OR arguments
-	 */
-	private static void printConnectionInfo(Socket socket) {
-		if (socket == null || socket.isClosed()) {
-			System.out.println(
-					"Error.  No active socket connection exists.");
-			return;
-		}
-		InetAddress remoteMachine = socket.getInetAddress();
-		String remoteHost = remoteMachine.getHostName();
-		System.out
-				.println("New socket connection with " + remoteHost);
-	}
 }
