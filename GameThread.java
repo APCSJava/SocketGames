33ac591347e9c152a2bacd53fda36f7665fd57d3
kentc14 <kentc14@10.30.4.66>
2017-12-14 13:40:58 +0000

    now with echo read/write

--- GameThread.java
+++ GameThread.java
@@ -1,7 +1,9 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.io.PrintWriter;
+import java.io.Reader;
 import java.net.Socket;
 
 /***
@@ -25,16 +27,13 @@ public class GameThread implements Runnable {
 	@SuppressWarnings("unchecked")
 	@Override
 	public void run() {
-		try (PrintWriter out = new PrintWriter(
-				socket.getOutputStream(), true);
-				BufferedReader br = new BufferedReader(
-						new InputStreamReader(
-								socket.getInputStream()))) {
+		try (PrintWriter out = new EchoWriter(socket.getOutputStream(), true);
+				BufferedReader br = new EchoReader(new InputStreamReader(socket.getInputStream()))) {
 			String gameMenu = GameTracker.buildGameListMenu();
 			out.println(); // provide some white space before menu
 			while (true) {
 				Thread.sleep(SCROLL_DELAY);
-				out.print(gameMenu);  // accept input on same line?
+				out.print(gameMenu); // accept input on same line?
 				out.flush();
 				String choice = br.readLine().trim().toLowerCase();
 				if ("q".equals(choice))
@@ -45,37 +44,69 @@ public class GameThread implements Runnable {
 					out.println(); // whitespace)
 					((Servable) o).serve(br, out);
 					Thread.sleep(SCROLL_DELAY);
-					out.println(
-							"\nThe game is over.  Press Enter to continue.");
+					out.println("\nThe game is over.  Press Enter to continue.");
 					br.readLine(); // provide opportunity for them to see msg
 					continue;
 				} else if (o instanceof Exception) {
-					out.println(
-							"An error occurred while attempting to start the game.");
+					out.println("An error occurred while attempting to start the game.");
 				} else {
 					out.println(choice + " is not a valid option.");
 				}
 				out.println("Press Enter/Return to continue.");
 				br.readLine(); // user is ready to continue
 			}
-			GameServer.LOGGER.info("Connection with "
-					+ socket.getInetAddress() + " closed normally.");
+			GameServer.LOGGER.info("Connection with " + socket.getInetAddress() + " closed normally.");
 
 		} catch (IOException |
 
 				NullPointerException e1) {
-			GameServer.LOGGER.warning(socket.getInetAddress()
-					+ " ended connection abruptly.");
+			GameServer.LOGGER.warning(socket.getInetAddress() + " ended connection abruptly.");
 		} catch (InterruptedException e) {
 			// Likely, because thread was interrupted while sleeping
-			GameServer.LOGGER.warning("Thread interrupted while sleeping."+e);
+			GameServer.LOGGER.warning("Thread interrupted while sleeping." + e);
 		} finally {
 			try {
 				socket.close();
 			} catch (IOException e) {
-				GameServer.LOGGER.info(
-						"GameThread attempting to close a closed socket");
+				GameServer.LOGGER.info("GameThread attempting to close a closed socket");
 			}
 		}
+	}
+
+	class EchoWriter extends PrintWriter {
+
+		public EchoWriter(OutputStream out, boolean autoFlush) {
+			super(out, autoFlush);
+		}
+
+		@Override
+		public void println(String s) {
+			super.println(s);
+			if (!s.trim().equals("")) {
+				System.out.println("Sending --> "+socket.getInetAddress()+" "+s.replace("\n", " "));
+			}
+		}
+
+	}
+	
+	class EchoReader extends BufferedReader {
+
+		public EchoReader(Reader in) {
+			super(in);
+		}
+		
+		@Override
+		public String readLine() {
+			try {
+				String s = super.readLine();
+				if (!s.trim().equals("")) {
+					System.out.println("Received <-- "+socket.getInetAddress()+" "+s.replace("\n", " "));
+				}
+				return s;
+			} catch (IOException e) {
+				return "";
+			}
+		}
+		
 	}
 }
