cae7e4144a254c16dda7b49c9fb462329248246e
Kent Collins <kent.r.collins@gmail.com>
2017-12-14 05:12:17 +0000

    sample demonstration of HiddenWord implementation

---
+++ HiddenWord.java
@@ -0,0 +1,48 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class HiddenWord implements Servable {
+	private String word;
+	private String hint;
+
+	public HiddenWord(String word) {
+		this.word = word;
+	}
+	
+	public HiddenWord() {
+		this.word = Dictionary.randomBySize(5).toUpperCase();
+		hint = "";
+		for (int i = 0; i<word.length(); i++) {
+			hint+="*";
+		}
+	}
+
+	public String getHint(String guess) {
+		String hint = "";
+
+		for (int i = 0; i < word.length(); i++) {
+			String guessLetter = guess.substring(i, i + 1);
+			if (word.substring(i, i + 1).equals(guessLetter))
+				hint += guessLetter;
+			else if (word.indexOf(guessLetter) != -1)
+				hint += "+";
+			else
+				hint += "*";
+		}
+
+		return hint;
+	}
+
+	@Override
+	public void serve(BufferedReader in, PrintWriter out)
+			throws IOException {
+		out.println("The word is "+word.length()+" letters long. Take a guess");
+		while (hint.contains("*")) {
+			String guess = in.readLine().trim().toUpperCase();
+			hint = getHint(guess);
+			out.println("Your hint is "+hint);
+		}
+		out.println("You guessed it!");
+	}
+}
