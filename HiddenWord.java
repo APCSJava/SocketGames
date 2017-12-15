290bfb19498de3c1927e57648be11b9298ce575b
Kent Collins <kentc14@hsfac_20741.local>
2017-12-15 13:59:21 +0000

    starter code for HiddenWord

---
+++ HiddenWord.java
@@ -0,0 +1,27 @@
+public class HiddenWord {
+	private String word;
+
+	public HiddenWord(String word) {
+		this.word = word;
+	}
+	
+
+	public String getHint(String guess) {
+		// must decide how to handle incorrect precondition
+
+		String hint = "";
+
+		for (int i = 0; i < word.length(); i++) {
+			String guessLetter = guess.substring(i, i + 1);
+			if (word.substring(i, i + 1).equals(guessLetter))
+				hint += guessLetter;
+			else if (word.indexOf(guessLetter) != -1)
+				hint += "+";
+			else
+				hint += "*";
+		}
+
+		return hint;
+	}
+
+}
