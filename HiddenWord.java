2a5f84342a3daa2c27e08f0e36a2375b822b0894
Kent Collins <kent.r.collins@gmail.com>
2017-12-16 10:05:04 +0000

    specified preconditions

--- HiddenWord.java
+++ HiddenWord.java
@@ -1,14 +1,14 @@
 public class HiddenWord {
 	private String word;
 
+	/** PRECONDITION: parameter is upper case */
 	public HiddenWord(String word) {
 		this.word = word;
 	}
-	
 
+	/** PRECONDITION: guess has same length as word, is upper case */
 	public String getHint(String guess) {
 		String hint = "";
-
 		for (int i = 0; i < word.length(); i++) {
 			String guessLetter = guess.substring(i, i + 1);
 			if (word.substring(i, i + 1).equals(guessLetter))
@@ -18,8 +18,6 @@ public class HiddenWord {
 			else
 				hint += "*";
 		}
-
 		return hint;
 	}
-
 }
