c500682c1570cc1c17a0d1464355ae87a836a252
Kent Collins <kent.r.collins@gmail.com>
2017-12-17 22:42:45 +0000

    updated documentation

--- GameThread.java
+++ GameThread.java
@@ -7,12 +7,16 @@ import java.io.Reader;
 import java.net.Socket;
 
 /***
+ * Manages the connection with a remote machine for the lifetime of the thread.
  * Sends menu options and reads the user's selection. If the selection is for a
- * specific game, serves the game. This thread loops until the selection "q" is
- * received, at which point the socket is closed and the thread terminates.
+ * specific game, serves the game; terminates the thread and closes the socket
+ * connection on receipt of a 'q'.
+ * 
+ * Extends PrintWriter and BufferedReader classes to echoed versions that
+ * read/write a copy of the input/output streams to the logger, as well.
  * 
  * @author kentcollins
- *
+ * @version Fall, 2017
  */
 
 public class GameThread implements Runnable {
@@ -27,8 +31,11 @@ public class GameThread implements Runnable {
 	@SuppressWarnings("unchecked")
 	@Override
 	public void run() {
-		try (PrintWriter out = new EchoWriter(socket.getOutputStream(), true);
-				BufferedReader br = new EchoReader(new InputStreamReader(socket.getInputStream()))) {
+		try (PrintWriter out = new EchoWriter(
+				socket.getOutputStream(), true);
+				BufferedReader br = new EchoReader(
+						new InputStreamReader(
+								socket.getInputStream()))) {
 			out.println(); // provide some white space before menu
 			while (true) {
 				Thread.sleep(SCROLL_DELAY);
@@ -39,39 +46,51 @@ public class GameThread implements Runnable {
 					break;
 				Object o = GameTracker.handleUserSelection(choice);
 				if (o instanceof Servable) {
-					out.println(GameTracker.getGameInfo((Class<Servable>) o.getClass()));
+					out.println(GameTracker.getGameInfo(
+							(Class<Servable>) o.getClass()));
 					out.println(); // whitespace)
 					((Servable) o).serve(br, out);
 					Thread.sleep(SCROLL_DELAY);
-					out.println("\nThe game is over.  Press Enter to continue.");
+					out.println(
+							"\nThe game is over.  Press Enter to continue.");
 					br.readLine(); // provide opportunity for them to see msg
 					continue;
 				} else if (o instanceof Exception) {
-					out.println("An error occurred while attempting to start the game.");
+					out.println(
+							"An error occurred while attempting to start the game.");
 				} else {
 					out.println(choice + " is not a valid option.");
 				}
 				out.println("Press Enter/Return to continue.");
 				br.readLine(); // user is ready to continue
 			}
-			GameServer.LOGGER.info("Connection with " + socket.getInetAddress() + " closed normally.");
+			GameServer.LOGGER.info("Connection with "
+					+ socket.getInetAddress() + " closed normally.");
 
 		} catch (IOException |
 
 				NullPointerException e1) {
-			GameServer.LOGGER.warning(socket.getInetAddress() + " ended connection abruptly.");
+			GameServer.LOGGER.warning(socket.getInetAddress()
+					+ " ended connection abruptly.");
 		} catch (InterruptedException e) {
 			// Likely, because thread was interrupted while sleeping
-			GameServer.LOGGER.warning("Thread interrupted while sleeping." + e);
+			GameServer.LOGGER.warning(
+					"Thread interrupted while sleeping." + e);
 		} finally {
 			try {
 				socket.close();
 			} catch (IOException e) {
-				GameServer.LOGGER.info("GameThread attempting to close a closed socket");
+				GameServer.LOGGER.info(
+						"GameThread attempting to close a closed socket");
 			}
 		}
 	}
 
+	/***
+	 * Extends PrintWriter to capture and log outgoing data.
+	 * 
+	 * @author kentcollins
+	 */
 	class EchoWriter extends PrintWriter {
 
 		public EchoWriter(OutputStream out, boolean autoFlush) {
@@ -82,25 +101,37 @@ public class GameThread implements Runnable {
 		public void println(String s) {
 			super.println(s);
 			if (!s.trim().equals("")) {
-				String first60 = "Sending --> "+socket.getInetAddress()+" "+s.replace("\n", " ").substring(0, Math.min(60, s.length()));
+				String first60 = "Sending --> "
+						+ socket.getInetAddress() + " "
+						+ s.replace("\n", " ").substring(0,
+								Math.min(60, s.length()));
 				System.out.println(first60);
 			}
 		}
 
 	}
-	
+
+	/***
+	 * Extends BufferedReader to capture and log incoming data.
+	 * 
+	 * @author kentcollins
+	 *
+	 */
 	class EchoReader extends BufferedReader {
 
 		public EchoReader(Reader in) {
 			super(in);
 		}
-		
+
 		@Override
 		public String readLine() {
 			try {
 				String s = super.readLine();
 				if (!s.trim().equals("")) {
-					String first60 = "Received <-- "+socket.getInetAddress()+" "+s.replace("\n", " ").substring(0, Math.min(60,  s.length()));
+					String first60 = "Received <-- "
+							+ socket.getInetAddress() + " "
+							+ s.replace("\n", " ").substring(0,
+									Math.min(60, s.length()));
 					System.out.println(first60);
 				}
 				return s;
@@ -108,6 +139,6 @@ public class GameThread implements Runnable {
 				return "";
 			}
 		}
-		
+
 	}
 }
