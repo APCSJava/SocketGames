ac49921a081980b88b21d149c779294e1a4179f6
Kent Collins <kent.r.collins@gmail.com>
2017-12-17 17:32:50 +0000

    clarified documentation

--- AbstractGame.java
+++ AbstractGame.java
@@ -1,5 +1,5 @@
 /***
- * Base class for games. 
+ * Base class for games.
  * 
  * @author kentcollins
  * @version Fall, 2017
@@ -7,18 +7,37 @@
  */
 public abstract class AbstractGame {
 
+	/**
+	 * Returns the highest score associated with this class, if present in the
+	 * GameTracker.
+	 * 
+	 * @return the current high score, if present; Integer.MIN_VALUE, otherwise
+	 */
 	public final int getHighScoreValue() {
 		return GameTracker.getHighScoreValue(this.getClass());
 	}
-	
+
+	/**
+	 * Returns the initials of the person who set the highest score, if set.
+	 * 
+	 * @return a three-letter, uppercase string; null, if no high score set
+	 */
 	public final String getHighScoreInitials() {
 		return GameTracker.getHighScoreInitials(this.getClass());
 	}
 
+	/**
+	 * Sets the high score in the GameTracker to the specified value.
+	 * @param value an integer score
+	 * @param initials the record holder
+	 */
 	public final void setHighScore(int value, String initials) {
-		if (initials== null || initials.equals("")) initials = "---";
-		else if (initials.length()>3) initials = initials.substring(0, 3).toUpperCase();
-		else initials = initials.toUpperCase();
+		if (initials == null || initials.equals(""))
+			initials = "---";
+		else if (initials.length() > 3)
+			initials = initials.substring(0, 3).toUpperCase();
+		else
+			initials = initials.toUpperCase();
 		GameTracker.setHighScore(this.getClass(), value, initials);
 	}
 }
