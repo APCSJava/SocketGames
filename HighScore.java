067db660c501da187c24882e0be7974cfcfc9a5e
Kent Collins <kent.r.collins@gmail.com>
2017-12-17 22:44:12 +0000

    modified documentation

--- HighScore.java
+++ HighScore.java
@@ -1,18 +1,25 @@
+/***
+ * Plain old Java object for associating a score with a player's initials.
+ * 
+ * @author kentcollins
+ * @version Fall, 2017
+ */
+
 public class HighScore {
-		private int score;
-		private String holder;
+	private int score;
+	private String holder;
 
-		public HighScore(int score, String holder) {
-			this.score = score;
-			this.holder = holder;
-		}
-
-		public int getScore() {
-			return score;
-		}
-
-		public String getInitials() {
-			return holder;
-		}
-
+	public HighScore(int score, String holder) {
+		this.score = score;
+		this.holder = holder;
 	}
+
+	public int getScore() {
+		return score;
+	}
+
+	public String getInitials() {
+		return holder;
+	}
+
+}
