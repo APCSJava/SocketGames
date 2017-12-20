40638533a1b8546d7cbfd1f3f7550cf5468bef8a
Kent Collins <kent.r.collins@gmail.com>
2017-12-20 09:02:14 +0000

    scoring added

--- GuessingGame.java
+++ GuessingGame.java
@@ -3,18 +3,20 @@ import java.io.IOException;
 import java.io.PrintWriter;
 
 @GameInfo(authors = {
-"Kent Collins" }, version = "Fall, 2017", gameTitle = "GuessingGame", description = "Completed version of HiddenWord FRQ.")
+		"Kent Collins" }, version = "Fall, 2017", gameTitle = "GuessingGame", description = "Completed version of HiddenWord FRQ.")
 
 public class GuessingGame extends AbstractGame implements Servable {
 	private String word;
+	private int numGuesses;
 
 	/** PRECONDITION: parameter is upper case */
 	public GuessingGame(String word) {
 		this.word = word;
 	}
-	
+
 	public GuessingGame() {
 		word = Dictionary.randomBySize(5, 8).toUpperCase();
+		numGuesses = 0;
 	}
 
 	/** PRECONDITION: guess has same length as word, is upper case */
@@ -35,15 +37,27 @@ public class GuessingGame extends AbstractGame implements Servable {
 	@Override
 	public void serve(BufferedReader in, PrintWriter out)
 			throws IOException {
-		out.println("The word is "+word.length()+" letters long.  Make a guess and I'll show you where you have the correct letters.  Letters that belong in the word but not in the given location will be marked by a '+'");
-		String guess ="";
+		out.println(
+				"Make a guess and I'll show you where you have the correct letters.  Letters that belong in the word but not in the given location will be marked by a '+'");
+		String guess = "";
 		while (!word.equals(guess)) {
-			while(guess == null || guess.equals("") || guess.length()!=word.length()) {
-				out.println("The word must be "+word.length()+ " letters long.");
+			while (guess == null || guess.equals("")
+					|| guess.length() != word.length()) {
+				out.println("Your guess should be " + word.length()
+						+ " letters long.");
 				guess = in.readLine().trim().toUpperCase();
 			}
+			numGuesses++;
 			out.println(getHint(guess));
 			guess = in.readLine().trim().toUpperCase();
+		}
+		int score = word.length() * numGuesses;
+		BestScore current = this.getBestScore();
+		if (current == null || score < current.getScore()) {
+			out.println(score
+					+ " is a new high score.  Please enter your initials...");
+			String inits = in.readLine().trim();
+			setRecord(score, inits);
 		}
 	}
 }
