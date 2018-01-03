528e08ec95617cc016823803d5c6e7d21ae43749
Kent Collins <kent.r.collins@gmail.com>
2018-01-03 20:54:17 +0000

    updated Nichomachus

--- Nicomachus.java
+++ Nicomachus.java
@@ -23,18 +23,36 @@ public class Nicomachus implements Servable {
 		output.println("Think of a number between 1 and 100 but don't tell me.  Press Enter to continue.");
 		input.readLine();
 		output.println("What is the remainder when you divide your number by 3?");
-		mod3 = Integer.parseInt(input.readLine().trim());
+		mod3 = protectedParse(input.readLine().trim());
+		if (mod3<0) {
+			output.println("Hmmm....I don't understand that input.  Try again later.");
+			return;
+		}
 		output.println("What is the remainder when you divide your number by 5?");
-		mod5 = Integer.parseInt(input.readLine().trim());
+		mod5 = protectedParse(input.readLine().trim());
+		if (mod5<0) {
+			output.println("Hmmm....I don't understand that input.  Try again later.");
+			return;
+		}
 		output.println("What is the remainder when you divide your number by 7?");
-		mod7 = Integer.parseInt(input.readLine().trim());
-		
-
-		output.println("Aha!  The number you selected was "+calc());
+		mod7 = protectedParse(input.readLine().trim());
+		if (mod7<0) {
+			output.println("Hmmm....I don't understand that input.  Try again later.");
+			return;
+		}
+		output.println("Aha!  The number you selected was "+calc()+".  How can I know that??!");
 	}
 	
 	private int calc() {
 		return (70*mod3+21*mod5+15*mod7)%105;
+	}
+	
+	private int protectedParse(String s) {
+		try {
+			return Integer.parseInt(s);
+		} catch (NumberFormatException e) {
+			return -1;
+		}
 	}
 
 	
