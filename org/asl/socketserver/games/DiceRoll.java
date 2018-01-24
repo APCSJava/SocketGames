ed916170f00c3c44c73ca3de571adbfcf87d4e52
DomA-asl <albertsdo@192.168.0.66>
2018-01-24 23:02:45 +0000

    Updated Games

--- org/asl/socketserver/games/DiceRoll.java
+++ org/asl/socketserver/games/DiceRoll.java
@@ -19,9 +19,9 @@ public class DiceRoll extends AbstractGame implements Servable{
 	}
 	
 	
-	public static String Message() {
-		String str = "This program simulates the rolling of a pair of dice./n";
-		str += "You enter the number of times you want the computer to roll the dice./n";
+	public static String message() {
+		String str = "This program simulates the rolling of a pair of dice.\n";
+		str += "You enter the number of times you want the computer to roll the dice.\n";
 		str += "Number of rolls: ";
 		return str;
 	}
@@ -35,38 +35,54 @@ public class DiceRoll extends AbstractGame implements Servable{
 		rollCount = totalRolls;
 	}
 	
+	public boolean isInteger(String str) {
+		
+		for(int i = 0; i<str.length(); i++) {
+			if(!(str.charAt(i)>=48&&str.charAt(i)<=57)) {
+				return false;
+			}
+		}
+		
+		return true;
+	}
+	
 	@Override
 	public void serve(BufferedReader input, PrintWriter output) throws IOException {
 		
-		Message();
+		output.println(message());
 	    
 	    String rollsStr = input.readLine();
-	    int numRolls = Integer.parseInt(rollsStr);
-	    output.println("Dice");
+	    if(isInteger(rollsStr)) {
+	    		int numRolls = Integer.parseInt(rollsStr);
+	    		output.println("Dice");
 	    
-	    setRollCount(numRolls);
+	    		setRollCount(numRolls);
 	    
-	    int[] outcomes = new int[11];
+	    		int[] outcomes = new int[11];
 
-	    for(int a = 0; a < rollCount; a++){
-	      int die1 = (int) (Math.random()*6+1);
-	      int die2 = (int) (Math.random()*6+1);
-	      int total = die1+die2;
-	      for(int b = 0; b < 11; b++) {
-	    	  	if(total==b+2) {
-	    	  		outcomes[b]++;
-	    	  	}
-	      }
-	    }
+	    		for(int a = 0; a < rollCount; a++){
+	    			int die1 = (int) (Math.random()*6+1);
+	    			int die2 = (int) (Math.random()*6+1);
+	    			int total = die1+die2;
+	    			for(int b = 0; b < 11; b++) {
+	    				if(total==b+2) {
+	    	  			outcomes[b]++;
+	    				}
+	    			}
+	    		}
 
-	    output.println("Total: Times Total Rolled, Frequency");
+	    		output.println("Total: Times Total Rolled, Frequency");
 	    
-	    for(int i = 0; i < 11; i++) {
-	    		double frequencyNum = frequency(outcomes[i]);
-	    		output.println("" + (i+2) + ": " + outcomes[i] + ", " + frequencyNum);
+	    		
+	    		for(int i = 0; i < 10; i++) {
+	    			double frequencyNum = frequency(outcomes[i]);
+	    			output.print((i+2) + ": " + outcomes[i] + ", " + frequencyNum + "\n");
+	    		}
+	    		output.println(12 + ": " + outcomes[10] + ", " + frequency(outcomes[10]));
+	    } else {
+	    		output.println("You must enter an integer. Thanks for trying!");
 	    }
-	    
-	  }
-		
 	}
+		
+}
 	
